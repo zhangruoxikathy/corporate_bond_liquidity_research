26500bc7cd51656effdc99eca8a3739c180cc053
zhangruoxikathy <ruoxizhang@uchicago.edu>
2024-02-16 17:12:41 -0600

    update load_wrds_k.py to personal branch

--- src/config.py
+++ src/config.py
@@ -24,15 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = config('DATA_DIR', default=(BASE_DIR / 'data'), cast=Path)
 OUTPUT_DIR = config('OUTPUT_DIR', default=(BASE_DIR / 'output'), cast=Path)
-WRDS_USERNAME = config("WRDS_USERNAME", default="")
+WRDS_USERNAME = config("WRDS_USERNAME", default="zhangruoxikathy")
 
 if __name__ == "__main__":
     
     ## If they don't exist, create the data and output directories
     (DATA_DIR / 'pulled').mkdir(parents=True, exist_ok=True)
-
-    # Sometimes, I'll create other folders to organize the data
-    # (DATA_DIR / 'intermediate').mkdir(parents=True, exist_ok=True)
-    # (DATA_DIR / 'derived').mkdir(parents=True, exist_ok=True)
 
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
---
+++ src/load_wrds_k.py
@@ -0,0 +1,124 @@
+
+'''
+Overview
+-------------
+This Python script aims to load corporate bond data from WRDSAPPS.BONDRET 
+on WRDS from 2002-07.
+ 
+Requirements
+-------------
+Access to the WRDS server and associated databases.
+
+Package versions 
+-------------
+pandas v1.4.4
+numpy v1.21.5
+wrds v3.1.2
+datetime v3.9.13
+csv v1.0
+gzip v3.9.13
+'''
+
+#* ************************************** */
+#* Libraries                              */
+#* ************************************** */ 
+import wrds
+import pandas as pd
+from pandas.tseries.offsets import MonthEnd, YearEnd
+import numpy as np
+import wrds
+import config
+from pathlib import Path
+
+
+OUTPUT_DIR = Path(config.OUTPUT_DIR)
+DATA_DIR = Path(config.DATA_DIR)
+WRDS_USERNAME = config.WRDS_USERNAME
+# START_DATE = config.START_DATE
+# END_DATE = config.END_DATE
+
+
+
+# https://wrds-www.wharton.upenn.edu/pages/get-data/wrds-bond-returns/wrds-bond-returns/
+description_bondret = {
+    "cusip": "CUSIP ID",
+    "date": "date",
+    "issue_id": "Mergent FISD Issue Id",
+    "bond_sym_id": "TRACE Bond Symbol",
+    "bsym": "Bloomberg Identifier",
+    "isin": "ISIN",
+    "company_symbol": "Company Symbol (issuer stock ticker)",
+    "bond_type": "Corporate Bond Types: Convertible, Debenture, Medium Term Note, MTN Zero",
+    "security_level": "Indicates if the security is a secured, senior or subordinated issue of the issuer",
+    "conv": "Flag Convertible",
+    "offering_date": "offering_date",
+    "offering_amt": "offering_amt",
+    "offering_price": "offering_price",
+    "principal_amt": "principal_amt",
+    "maturity": "maturity",
+    "treasury_maturity": "treasury_maturity",
+    "coupon": "coupon",
+    "day_count_basis": "day_count_basis",
+    "dated_date": "dated_date",
+    "first_interest_date": "first_interest_date",
+    "last_interest_date": "last_interest_date",
+    "ncoups": "ncoups",
+    "amount_outstanding": "amount_outstanding",
+    "r_sp": "r_sp",
+    "r_mr": "r_mr",
+    "r_fr": "r_fr",
+    "n_sp": "n_sp",
+    "n_mr": "n_mr",
+    "n_fr": "n_fr",
+    "rating_num": "rating_num"
+}
+
+
+def pull_bondret(wrds_username=WRDS_USERNAME):
+    """
+    Select  
+    See description_compustat for a description of the variables.
+    """
+    sql_query = """
+        SELECT 
+            cusip, date, issue_id, bond_sym_id, bsym, isin, company_symbol, bond_type,
+            security_level, conv, offering_date, offering_amt, offering_price,
+            principal_amt, maturity, treasury_maturity, coupon, day_count_basis,
+            dated_date, first_interest_date, last_interest_date, ncoups,
+            amount_outstanding, r_sp, r_mr, r_fr, n_sp, n_mr, n_fr, rating_num
+        FROM 
+        
+            WRDSAPPS.BONDRET
+        WHERE 
+            date >= '07/01/2002'
+        """
+    # with wrds.Connection(wrds_username=wrds_username) as db:
+    #     comp = db.raw_sql(sql_query, date_cols=["datadate"])
+    db = wrds.Connection(wrds_username=WRDS_USERNAME)
+    bond = db.raw_sql(sql_query, date_cols=["date"])
+    db.close()
+
+    bond["year"] = bond["date"].dt.year
+    return bond
+
+
+# ISSUE_ID, bond_sym_id, bsym, ISIN, company_symbol, BOND_TYPE,
+#             SECURITY_LEVEL, CONV, OFFERING_DATE, OFFERING_AMT, OFFERING_PRICE, PRINCIPAL_AMT,
+#             MATURITY, TREASURY_MATURITY, COUPON, DAY_COUNT_BASIS, DATED_DATE,
+#             FIRST_INTEREST_DATE, LAST_INTEREST_DATE, NCOUPS, AMOUNT_OUTSTANDING, R_SP, R_MR,
+#             R_FR, N_SP, N_MR, N_FR, RATING_NUM 
+
+
+def load_bondret(data_dir=DATA_DIR):
+    path = Path(data_dir) / "pulled" / "Bondret.parquet"
+    bond = pd.read_parquet(path)
+    return bond
+
+
+def _demo():
+    comp = load_bondret(data_dir=DATA_DIR)
+
+
+if __name__ == "__main__":
+    comp = pull_bondret(wrds_username=WRDS_USERNAME)
+    comp.to_parquet(DATA_DIR / "pulled" / "Bondret.parquet")
