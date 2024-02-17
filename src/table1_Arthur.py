d31197676eb1213c368fe966a70e2b2a910a1a2d
ajfly <jijie1279@gmail.com>
2024-02-17 17:21:37 -0600

    update table 1 sample selection (problematic)

--- src/table1_Arthur.py
+++ src/table1_Arthur.py
@@ -9,7 +9,9 @@ df = load_wrds_k.load_bondret(data_dir = DATA_DIR)
 
 def sample_data(df):
 
-    # select data from 2003-04-14 to 2009-6-30 
+    # following the data clean setups in the paper 
+
+    # select Phase I and II bonds from 2003-04-14 to 2009-6-30 
     df['date'] = pd.to_datetime(df['date'])
     start_date = '2003-04-14'
     end_date = '2009-06-30'
@@ -20,20 +22,24 @@ def sample_data(df):
     # drop all bonds that only exist after the date of phase 3: Feb 7 2005
 
     cutoff_date = pd.Timestamp('2005-02-07')
-
     df = df.groupby('cusip').filter(lambda x: x['date'].min() <= cutoff_date)
 
     # make sure it exist for at least one full year 
     df = df.groupby('cusip').filter(lambda x: x['date'].max() - x['date'].min() >= pd.Timedelta(days=365))
 
     # keep only investment grade
-    df = df[df['rating_num'] <= 10]
+    df_sample = df[df['rating_num'] <= 10]
 
-    return df 
+    return df_sample
 
 
-df_filtered = sample_data(df)
+df_sample = sample_data(df)
 
-print("Number of bonds in the sample: " + str(len(df_filtered['cusip'].unique())))
+df_sample_2003 = df_sample[df_sample['year'] == 2003]
+df_all_2003 = df[df['year'] == 2003 ]
 
-# gives an output of 6627   vs  1035 in the paper!!! 
+
+print("Number of bonds in the sample in 2003: " + str(len(df_sample_2003['cusip'].unique())))
+print("Number of all bonds in 2003: " + str(len(df_all_2003['cusip'].unique())))
+
+# gives an output of 5560 and 9875, respectively;  in this paper they are 744 and 4161, respectively
