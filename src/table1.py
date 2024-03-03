cf1546128a8106087f6c75486a8ba38a61c1f2b0
hantaoxiao <112460623+hantaoxiao@users.noreply.github.com>
2024-03-03 14:44:48 -0600

    master function

--- src/table1.py
+++ src/table1.py
@@ -5,6 +5,7 @@ import load_wrds_bondret
 import load_opensource
 import load_intraday
 import data_processing
+import datetime
 
 OUTPUT_DIR = config.OUTPUT_DIR
 DATA_DIR = config.DATA_DIR
@@ -20,8 +21,8 @@ df_daily = pd.read_csv('/Users/adair/Desktop/FinancialTool/Group_Project/BondDai
 df_intraday = pd.read_parquet(DATA_DIR / "pulled" / load_intraday.FILE_NAME)
 
 # pre-processing the data
-df_all = data_processing.all_trace_data_merge(df_daily, df_bondret)   #this is the dataset for panel B in table 1 
-df_sample = data_processing.sample_selection(df_all) # this is the dataset for panel A in table 1
+df_all = data_processing.all_trace_data_merge(df_daily, df_bondret, end_date = datetime.today().strftime('%Y-%m-%d'))   #this is the dataset for panel B in table 1 
+df_sample = data_processing.sample_selection(df_all, end_date = datetime.today().strftime('%Y-%m-%d')) # this is the dataset for panel A in table 1
 
 # Give df_all and df_sample a month column
 df_all['month'] = df_all['date'].dt.month
@@ -105,133 +106,143 @@ def cal_count(dataframe, column='cusip'):
     count.set_index('year', inplace=True)
     return count
 
-
-# Calculate the number of unique cusips in df_sample and df_all
-df_sample_cusip = cal_count(df_sample)
-df_all_cusip = cal_count(df_all)
-
-
-# Calculate the Issuance of df_sample and df_all
-df_sample['issuance'] = df_sample['offering_amt'] * df_sample['principal_amt'] * \
-                        df_sample['offering_price'] / 100 / 1000000
-
-df_sample_issuance = pd.concat([cal_avrage(df_sample, 'issuance'), \
-                    cal_median(df_sample, 'issuance'), cal_std(df_sample, 'issuance')], axis=1)
-
-df_all['issuance'] = df_all['offering_amt'] * df_all['principal_amt'] * \
-                     df_all['offering_price'] / 100 / 1000000
-
-df_all_issuance = pd.concat([cal_avrage(df_all, 'issuance'), \
-                    cal_median(df_all, 'issuance'), cal_std(df_all, 'issuance')], axis=1)
-
-# Calculate the Moondy Rating of df_sample and df_all
-df_sample_moody = pd.concat([cal_avrage(df_sample, 'n_mr'), \
-                    cal_median(df_sample, 'n_mr'), cal_std(df_sample, 'n_mr')], axis=1)
-
-df_all_moody = pd.concat([cal_avrage(df_all, 'n_mr'), \
-                    cal_median(df_all, 'n_mr'), cal_std(df_all, 'n_mr')], axis=1)
-
-# Calculate the Maturity of df_sample and df_all
-df_sample_maturity = pd.concat([cal_avrage(df_sample, 'tmt'), \
-                    cal_median(df_sample, 'tmt'), cal_std(df_sample, 'tmt')], axis=1)
-
-df_all_maturity = pd.concat([cal_avrage(df_all, 'tmt'), \
-                    cal_median(df_all, 'tmt'), cal_std(df_all, 'tmt')], axis=1)
-
-# Calculate the coupon of df_sample and df_all
-df_sample_coupon = pd.concat([cal_avrage(df_sample, 'coupon_y'), \
-                    cal_median(df_sample, 'coupon_y'), cal_std(df_sample, 'coupon_y')], axis=1)
-
-df_all_coupon = pd.concat([cal_avrage(df_all, 'coupon_y'), \
-                    cal_median(df_all, 'coupon_y'), cal_std(df_all, 'coupon_y')], axis=1)
-
-# Calculate the age where the gap between the issuance date and the trade date in years
-df_sample[['date', 'offering_date']] = df_sample[['date', 'offering_date']].apply(pd.to_datetime)
-df_all[['date', 'offering_date']] = df_all[['date', 'offering_date']].apply(pd.to_datetime)
-
-df_sample['age'] = (df_sample['date'] - df_sample['offering_date']).dt.days / 365
-df_all['age'] = (df_all['date'] - df_all['offering_date']).dt.days / 365
-
-df_sample_age = pd.concat([cal_avrage(df_sample, 'age'), \
-                    cal_median(df_sample, 'age'), cal_std(df_sample, 'age')], axis=1)
-
-df_all_age = pd.concat([cal_avrage(df_all, 'age'), \
-                    cal_median(df_all, 'age'), cal_std(df_all, 'age')], axis=1)
-
-# Calculate the turnover in df_sample and df_all
-df_sample['turnover'] = df_sample['t_volume'] / df_sample['issuance'] / 10000
-df_all['turnover'] = df_all['t_volume'] / df_all['issuance'] / 10000
-
-df_sample_turnover = pd.concat([cal_avrage(df_sample, 'turnover'), \
-                    cal_median(df_sample, 'turnover'), cal_std(df_sample, 'turnover')], axis=1)
-
-df_all_turnover = pd.concat([cal_avrage(df_all, 'turnover'), \
-                    cal_median(df_all, 'turnover'), cal_std(df_all, 'turnover')], axis=1)
+def calculation(df_sample, df_all):
+    # Calculate the number of unique cusips in df_sample and df_all
+    df_sample_cusip = cal_count(df_sample)
+    df_all_cusip = cal_count(df_all)
 
 
-# Calculate the return of df_sample and df_all
+    # Calculate the Issuance of df_sample and df_all
+    df_sample['issuance'] = df_sample['offering_amt'] * df_sample['principal_amt'] * \
+                            df_sample['offering_price'] / 100 / 1000000
 
-# We need to drop teh duplicate entires in df_sample and df_all
-df_sample_month = df_sample.drop_duplicates(subset=['cusip', 'year', 'month']).reset_index()
-df_all_month = df_all.drop_duplicates(subset=['cusip', 'year', 'month']).reset_index()
+    df_sample_issuance = pd.concat([cal_avrage(df_sample, 'issuance'), \
+                        cal_median(df_sample, 'issuance'), cal_std(df_sample, 'issuance')], axis=1)
 
-df_sample_month['return'] = np.log(df_sample_month['price_eom'] / \
-                            df_sample_month.groupby(['cusip'])['price_eom'].shift(1)) * 100
+    df_all['issuance'] = df_all['offering_amt'] * df_all['principal_amt'] * \
+                        df_all['offering_price'] / 100 / 1000000
 
-df_all_month['return'] = np.log(df_all_month['price_eom'] / \
-                            df_all_month.groupby(['cusip'])['price_eom'].shift(1)) * 100
+    df_all_issuance = pd.concat([cal_avrage(df_all, 'issuance'), \
+                        cal_median(df_all, 'issuance'), cal_std(df_all, 'issuance')], axis=1)
+
+    # Calculate the Moondy Rating of df_sample and df_all
+    df_sample_moody = pd.concat([cal_avrage(df_sample, 'n_mr'), \
+                        cal_median(df_sample, 'n_mr'), cal_std(df_sample, 'n_mr')], axis=1)
+
+    df_all_moody = pd.concat([cal_avrage(df_all, 'n_mr'), \
+                        cal_median(df_all, 'n_mr'), cal_std(df_all, 'n_mr')], axis=1)
+
+    # Calculate the Maturity of df_sample and df_all
+    df_sample_maturity = pd.concat([cal_avrage(df_sample, 'tmt'), \
+                        cal_median(df_sample, 'tmt'), cal_std(df_sample, 'tmt')], axis=1)
+
+    df_all_maturity = pd.concat([cal_avrage(df_all, 'tmt'), \
+                        cal_median(df_all, 'tmt'), cal_std(df_all, 'tmt')], axis=1)
+
+    # Calculate the coupon of df_sample and df_all
+    df_sample_coupon = pd.concat([cal_avrage(df_sample, 'coupon_y'), \
+                        cal_median(df_sample, 'coupon_y'), cal_std(df_sample, 'coupon_y')], axis=1)
+
+    df_all_coupon = pd.concat([cal_avrage(df_all, 'coupon_y'), \
+                        cal_median(df_all, 'coupon_y'), cal_std(df_all, 'coupon_y')], axis=1)
+
+    # Calculate the age where the gap between the issuance date and the trade date in years
+    df_sample[['date', 'offering_date']] = df_sample[['date', 'offering_date']].apply(pd.to_datetime)
+    df_all[['date', 'offering_date']] = df_all[['date', 'offering_date']].apply(pd.to_datetime)
+
+    df_sample['age'] = (df_sample['date'] - df_sample['offering_date']).dt.days / 365
+    df_all['age'] = (df_all['date'] - df_all['offering_date']).dt.days / 365
+
+    df_sample_age = pd.concat([cal_avrage(df_sample, 'age'), \
+                        cal_median(df_sample, 'age'), cal_std(df_sample, 'age')], axis=1)
+
+    df_all_age = pd.concat([cal_avrage(df_all, 'age'), \
+                        cal_median(df_all, 'age'), cal_std(df_all, 'age')], axis=1)
+
+    # Calculate the turnover in df_sample and df_all
+    df_sample['turnover'] = df_sample['t_volume'] / df_sample['issuance'] / 10000
+    df_all['turnover'] = df_all['t_volume'] / df_all['issuance'] / 10000
+
+    df_sample_turnover = pd.concat([cal_avrage(df_sample, 'turnover'), \
+                        cal_median(df_sample, 'turnover'), cal_std(df_sample, 'turnover')], axis=1)
+
+    df_all_turnover = pd.concat([cal_avrage(df_all, 'turnover'), \
+                        cal_median(df_all, 'turnover'), cal_std(df_all, 'turnover')], axis=1)
 
 
-# group by year and cusip and calculate the weighted average return
-df_sample_month_grouped = df_sample_month.groupby(['year', 'cusip'])['return'].mean().reset_index(name='Avg_return')
-df_all_month_grouped = df_all_month.groupby(['year', 'cusip'])['return'].mean().reset_index(name='Avg_return')
+    # Calculate the return of df_sample and df_all
+
+    # We need to drop teh duplicate entires in df_sample and df_all
+    df_sample_month = df_sample.drop_duplicates(subset=['cusip', 'year', 'month']).reset_index()
+    df_all_month = df_all.drop_duplicates(subset=['cusip', 'year', 'month']).reset_index()
+
+    df_sample_month['return'] = np.log(df_sample_month['price_eom'] / \
+                                df_sample_month.groupby(['cusip'])['price_eom'].shift(1)) * 100
+
+    df_all_month['return'] = np.log(df_all_month['price_eom'] / \
+                                df_all_month.groupby(['cusip'])['price_eom'].shift(1)) * 100
 
 
-df_sample_return = pd.concat([cal_avrage(df_sample_month_grouped, 'Avg_return'), \
-                    cal_median(df_sample_month_grouped, 'Avg_return'), cal_std(df_sample_month_grouped, 'Avg_return')], axis=1)
-
-df_all_return = pd.concat([cal_avrage(df_all_month_grouped, 'Avg_return'), \
-                    cal_median(df_all_month_grouped, 'Avg_return'), cal_std(df_all_month_grouped, 'Avg_return')], axis=1)
-
-# Calculate the volatility of df_sample and df_all
-
-df_sample_vol_grouped = df_sample_month.groupby(['year', 'cusip'])['return'].std().reset_index(name='volatility')
-df_all_vol_grouped = df_all_month.groupby(['year', 'cusip'])['return'].std().reset_index(name='volatility')
-
-df_sample_vol = pd.concat([cal_avrage(df_sample_vol_grouped, 'volatility'), \
-                    cal_median(df_sample_vol_grouped, 'volatility'), cal_std(df_sample_vol_grouped, 'volatility')], axis=1)
-
-df_all_vol = pd.concat([cal_avrage(df_all_vol_grouped, 'volatility'), \
-                    cal_median(df_all_vol_grouped, 'volatility'), cal_std(df_all_vol_grouped, 'volatility')], axis=1)
-
-# Calculate the Price in df_sample and df_all
-df_sample_month_price = df_sample.groupby(['year', 'cusip', 'date'])['prclean'].mean().reset_index()
-df_all_month_price = df_all.groupby(['year', 'cusip', 'date'])['prclean'].mean().reset_index()
-
-df_sample_price = pd.concat([cal_avrage(df_sample_month_price, 'prclean'), \
-                    cal_median(df_sample_month_price, 'prclean'), cal_std(df_sample_month_price, 'prclean')], axis=1)
-
-df_all_price = pd.concat([cal_avrage(df_all_month_price, 'prclean'), \
-                    cal_median(df_all_month_price, 'prclean'), cal_std(df_all_month_price, 'prclean')], axis=1)
-
-# Calculate the number of trades in df_sample and df_all
-df_sample_trade = pd.concat([cal_avrage(df_sample_month, '#trade'), \
-                    cal_median(df_sample_month, '#trade'), cal_std(df_sample_month, '#trade')], axis=1)
-
-df_all_trade = pd.concat([cal_avrage(df_all_month, '#trade'), \
-                    cal_median(df_all_month, '#trade'), cal_std(df_all_month, '#trade')], axis=1)
+    # group by year and cusip and calculate the weighted average return
+    df_sample_month_grouped = df_sample_month.groupby(['year', 'cusip'])['return'].mean().reset_index(name='Avg_return')
+    df_all_month_grouped = df_all_month.groupby(['year', 'cusip'])['return'].mean().reset_index(name='Avg_return')
 
 
-# Calculate the Trade_size in df_sample and df_all
-df_sample_month['trade_size'] = df_sample_month['t_dvolume'] / df_sample_month['#trade'] / 1000
-df_all_month['trade_size'] = df_all_month['t_dvolume'] / df_all_month['#trade'] / 1000
+    df_sample_return = pd.concat([cal_avrage(df_sample_month_grouped, 'Avg_return'), \
+                        cal_median(df_sample_month_grouped, 'Avg_return'), cal_std(df_sample_month_grouped, 'Avg_return')], axis=1)
 
-df_sample_size = pd.concat([cal_avrage(df_sample_month, 'trade_size'), \
-                    cal_median(df_sample_month, 'trade_size'), cal_std(df_sample_month, 'trade_size')], axis=1)
+    df_all_return = pd.concat([cal_avrage(df_all_month_grouped, 'Avg_return'), \
+                        cal_median(df_all_month_grouped, 'Avg_return'), cal_std(df_all_month_grouped, 'Avg_return')], axis=1)
 
-df_all_size = pd.concat([cal_avrage(df_all_month, 'trade_size'), \
-                    cal_median(df_all_month, 'trade_size'), cal_std(df_all_month, 'trade_size')], axis=1)
+    # Calculate the volatility of df_sample and df_all
 
+    df_sample_vol_grouped = df_sample_month.groupby(['year', 'cusip'])['return'].std().reset_index(name='volatility')
+    df_all_vol_grouped = df_all_month.groupby(['year', 'cusip'])['return'].std().reset_index(name='volatility')
+
+    df_sample_vol = pd.concat([cal_avrage(df_sample_vol_grouped, 'volatility'), \
+                        cal_median(df_sample_vol_grouped, 'volatility'), cal_std(df_sample_vol_grouped, 'volatility')], axis=1)
+
+    df_all_vol = pd.concat([cal_avrage(df_all_vol_grouped, 'volatility'), \
+                        cal_median(df_all_vol_grouped, 'volatility'), cal_std(df_all_vol_grouped, 'volatility')], axis=1)
+
+    # Calculate the Price in df_sample and df_all
+    df_sample_month_price = df_sample.groupby(['year', 'cusip', 'date'])['prclean'].mean().reset_index()
+    df_all_month_price = df_all.groupby(['year', 'cusip', 'date'])['prclean'].mean().reset_index()
+
+    df_sample_price = pd.concat([cal_avrage(df_sample_month_price, 'prclean'), \
+                        cal_median(df_sample_month_price, 'prclean'), cal_std(df_sample_month_price, 'prclean')], axis=1)
+
+    df_all_price = pd.concat([cal_avrage(df_all_month_price, 'prclean'), \
+                        cal_median(df_all_month_price, 'prclean'), cal_std(df_all_month_price, 'prclean')], axis=1)
+
+    # Calculate the number of trades in df_sample and df_all
+    df_sample_trade = pd.concat([cal_avrage(df_sample_month, '#trade'), \
+                        cal_median(df_sample_month, '#trade'), cal_std(df_sample_month, '#trade')], axis=1)
+
+    df_all_trade = pd.concat([cal_avrage(df_all_month, '#trade'), \
+                        cal_median(df_all_month, '#trade'), cal_std(df_all_month, '#trade')], axis=1)
+
+
+    # Calculate the Trade_size in df_sample and df_all
+    df_sample_month['trade_size'] = df_sample_month['t_dvolume'] / df_sample_month['#trade'] / 1000
+    df_all_month['trade_size'] = df_all_month['t_dvolume'] / df_all_month['#trade'] / 1000
+
+    df_sample_size = pd.concat([cal_avrage(df_sample_month, 'trade_size'), \
+                        cal_median(df_sample_month, 'trade_size'), cal_std(df_sample_month, 'trade_size')], axis=1)
+
+    df_all_size = pd.concat([cal_avrage(df_all_month, 'trade_size'), \
+                        cal_median(df_all_month, 'trade_size'), cal_std(df_all_month, 'trade_size')], axis=1)
+
+    return df_sample_cusip, df_sample_issuance, df_sample_moody, df_sample_maturity, df_sample_coupon, \
+            df_sample_age, df_sample_turnover, df_sample_size, df_sample_trade, df_sample_return, df_sample_vol, df_sample_price, \
+            df_all_cusip, df_all_issuance, df_all_moody, df_all_maturity, df_all_coupon, \
+            df_all_age, df_all_turnover, df_all_size, df_all_trade, df_all_return, df_all_vol, df_all_price
+
+
+df_sample_cusip, df_sample_issuance, df_sample_moody, df_sample_maturity, df_sample_coupon, \
+df_sample_age, df_sample_turnover, df_sample_size, df_sample_trade, df_sample_return, df_sample_vol, df_sample_price, \
+df_all_cusip, df_all_issuance, df_all_moody, df_all_maturity, df_all_coupon, \
+df_all_age, df_all_turnover, df_all_size, df_all_trade, df_all_return, df_all_vol, df_all_price = calculation(df_sample, df_all)
 
 
 # concat all results of df_sample and df_all
