32692570999d9f17e42c7aefc2b44cd685001ad1
hunterY <hyoung3@gmail.com>
2024-03-03 09:33:49 -0600

    update to load_TRACE file

--- src/load_trace.py
+++ src/load_trace.py
@@ -1,25 +1,22 @@
 from pathlib import Path
 import pandas as pd
-from MakeIntra_Daily_v2_functions import (
-    pull_mergent_files, clean_and_filter_mergent_files, pull_TRACE, clean_TRACE)
+from Intraday_TRACE_Pull import pull_TRACE, compile_TRACE
 import config
 
 OUTPUT_DIR = Path(config.OUTPUT_DIR)
 DATA_DIR = Path(config.DATA_DIR)
+FILE_NAME = 'IntradayTRACE.parquet'
 
 
 def pull_intraday_TRACE():
-    pull_mergent_files()
-    clean_and_filter_mergent_files()
     pull_TRACE()
-    clean_TRACE()
-    # filter_and_save_intraday_TRACE()
+    return compile_TRACE()
 
 
 def load_intraday_TRACE(data_dir=DATA_DIR):
-    path = data_dir / "pulled" / "intraday_TRACE_filtered.parquet"
+    path = data_dir.joinpath(f"pulled/{FILE_NAME}")
     if not path.exists():
-        path = data_dir / "manual" / "intraday_TRACE_filtered.parquet"
+        path = data_dir.joinpath(f"manual/{FILE_NAME}")
     df = pd.read_parquet(path)
     return df
 
@@ -30,3 +27,4 @@ def _demo():
 
 if __name__ == '__main__':
     trade_data = pull_intraday_TRACE()
+    trade_data.to_parquet(config.DATA_DIR.joinpath('pulled/intraday_TRACE_filtered.parquet'))
